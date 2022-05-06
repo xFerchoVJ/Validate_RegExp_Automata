175d28f8732266fbf69ac35e4d9ebad4fa08add7
xFerchoVJ <fer_elsword@hotmail.com>
2022-05-06 15:57:38 -0500

    Change json return

--- backend/app/controllers/api/v1/validation_controller.rb
+++ backend/app/controllers/api/v1/validation_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::ValidationController < ApplicationController
     reg_exp = /^a*(b|ca*)$/
     correct_expresion = params[:string] =~ reg_exp
       if correct_expresion
-        render json: { message: "La expresion ingresada es valida..." }.to_json
+        render json: { message: "La expresion ingresada es valida...", error: false}.to_json
       else
-        render json: { message: "La expresion ingresada es invalida..." }.to_json
+        render json: { message: "La expresion ingresada es invalida...", error: true}.to_json
       end
   end
   
